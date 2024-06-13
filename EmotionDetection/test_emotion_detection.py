84be17dab11e602f2330a9a4a8f961dbad94c747
Sahar Cohen <saharcohen456@gmail.com>
2024-06-13 13:01:49 -0400

    save

--- EmotionDetection/test_emotion_detection.py
+++ EmotionDetection/test_emotion_detection.py
@@ -2,19 +2,18 @@ import unittest
 from emotion_detection import emotion_detector
 class TestEmotionDetection(unittest.TestCase):
     def test_joy(self):
-        self.assertEqual(emotion_detector("I am glad this happened").dominant_emotion,"joy")
+        self.assertEqual(emotion_detector("I am glad this happened")['dominant_emotion'],"joy")
 
     def test_anger(self):
-        self.assertEqual(emotion_detector("I am really mad about this").dominant_emotion,"anger")
+        self.assertEqual(emotion_detector("I am really mad about this")['dominant_emotion'],"anger")
     
     def test_disgust(self):
-        self.assertEqual(emotion_detector("I feel disgusted just hearing about this").dominant_emotion,"disgust")
+        self.assertEqual(emotion_detector("I feel disgusted just hearing about this")['dominant_emotion'],"disgust")
     
     def test_sadness(self):
-        self.assertEqual(emotion_detector("I am so sad about this").dominant_emotion,"sadness")
+        self.assertEqual(emotion_detector("I am so sad about this")['dominant_emotion'],"sadness")
     
     def test_fear(self):
-        self.assertEqual(emotion_detector("I am really afraid that this will happen").dominant_emotion,"fear")
+        self.assertEqual(emotion_detector("I am really afraid that this will happen")['dominant_emotion'],"fear")
     
-if __name__ in "__name__":
-    unittest.main()
+unittest.main()
